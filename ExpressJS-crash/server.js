34ce3176931797a9889e5040b4d1938e9dc1faef
fredcodes <tihomirov.fred03@gmail.com>
2026-07-25 15:15:41 +0300

    Added env and request parameters

--- ExpressJS-crash/server.js
+++ ExpressJS-crash/server.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+let posts = [
+    {id: 1, title: 'Post One'},
+    {id: 2, title: 'Post Two'},
+    {id: 3, title: 'Post Three'},
+];
+
+// Get all posts
+app.get('/api/posts', (req, res) => {
+    const limit = parseInt(req.query.limit);
+
+    if(!isNaN(limit) && limit > 0) {
+        res.json(posts.slice(0, limit));
+    } else {
+        res.json(posts);
+    }
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+// Get single post
+app.get('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    res.json(posts.filter((post) => post.id === id));
 });
 
-app.listen(8000, () => console.log('Server is running on port 8000'));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
