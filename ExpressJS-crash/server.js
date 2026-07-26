531a7e1f92e5a70f0ac3fbf80e8e28f7b7dc67fa
fredcodes <tihomirov.fred03@gmail.com>
2026-07-26 20:30:50 +0300

    Added error handling

--- ExpressJS-crash/server.js
+++ ExpressJS-crash/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import path from 'path';
 import posts from './routes/posts.js';
+import logger from './middleware/logger.js'
+import errorHandler from './middleware/error.js'
+import notFound from './middleware/notFound.js'
 
 const port = process.env.PORT || 8000;
 const app = express();
@@ -9,8 +12,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// logger middleware
+app.use(logger);
+
 // Routes
 app.use('/api/posts', posts);
+
+// Error handler
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
