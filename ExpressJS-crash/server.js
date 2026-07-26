be3df0f137f59387be2bb354ad5eefc7d2a452a3
fredcodes <tihomirov.fred03@gmail.com>
2026-07-26 21:24:58 +0300

    Final changes, updated frontend

--- ExpressJS-crash/server.js
+++ ExpressJS-crash/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js'
 import errorHandler from './middleware/error.js'
 import notFound from './middleware/notFound.js'
-
 const port = process.env.PORT || 8000;
+
+// get dir name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 // Body parser middleware
@@ -14,6 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 
 // logger middleware
 app.use(logger);
+
+// setup static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/posts', posts);
